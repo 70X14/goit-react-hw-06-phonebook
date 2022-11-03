53d07fdb4c0b0cc4d5eb2491d31e87a5b8627fd4
Vlad Guminyuk <vldgum@gmail.com>
2022-11-03 13:20:26 +0200

    fix

--- src/redux/slices/sliceContacts.js
+++ src/redux/slices/sliceContacts.js
@@ -1,10 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const contactsInitialState = [
-	{ id: 'id-1', name: 'Саня', number: '111-22-33' },
-	{ id: 'id-2', name: 'Стас', number: '444-55-66' },
-	{ id: 'id-3', name: 'Рома', number: '777-88-99' },
-	{ id: 'id-4', name: 'Коваль', number: '000-00-00' },
+	{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+	{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+	{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+	{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
 
   export const contactsFormSlice = createSlice({
